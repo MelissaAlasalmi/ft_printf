86fe307427a9b936926fee856860498790c07e0a
Melissa Alasalmi <malasalm@c2r4p7.hive.fi>
2020-02-19 15:37:13 +0200

    notes

--- ft_printf.h
+++ ft_printf.h
@@ -9,23 +9,25 @@
 typedef	struct	s_printf
 {
 	//flags
-	int			hash; // Alternates. For f flag, the output will always contain a decimal point. For o, x, and X flags, the texts 0, 0x, and 0X are added to non-zero numbers.
-	int			zero; //When the 'width' is specified, adds the width amount of zeros in front of numeric types
-	int			minus; //specifies left adjustment of the onverted argument
-	int			plus; //adds a plus for positive or a minus for negative-signed numeric types
-	int			space; //adds a space for positive signed-numeric types, unless there is a plus!
+	int			hash; // For f flag, the output will always contain a decimal point even if the fractional part of the number is zero. For o, x, X flags: 0, 0x, 0X are added to the front of the number.
+	int			zero; // When the 'width' is specified, adds the 'widths' amount of zeros in front of the number (operates only on numbers)
+	int			minus; // Left-aligns the output (operates on all types)
+	int			plus; // Adds a plus to the start of numbers when positive. (operates only on numeric types)
+	int			space; // Adds a space to the start of numbers when positive... unless there is a plus! (operates only on numeric types)
 
 	//width
-	int			asterisk;
+	int			len; //The minimum number of characters to allocate for the output.
+	int			asterisk; //The precision is specified in an integer argument to printf preceding the number to be formatted. (operates on all types)
 
-	//precision
-	int			decimal;
+	//precision - 
+	int			percision; // The minimum number of characters to be output, padded on the left with 0’s if necessary.
+	int			decimal; // For floats, specifies the maximum number of ints to output after a decimal point to the right of a number. The output is rounded.
 
-	//length
-	int			hh;
-	int			h;
-	int			l;
-	int			ll;
+	//length - Unlike flags, the length flag dosen't tell printf how to display the number, but more about the number itself.
+	int			hh; // +d = signed char, +i = signed char, +o = unsigned char, +u = unsigned char, +x = unsigned char, +X = unsigned char
+	int			h; //+d = signed short, +i = signed short, +o = unsigned short, +u = unsigned short, +x = unsigned short, +X = unsigned short
+	int			l; // +d = long, +i = long, +o = unsigned long, +u = unsigned long, +x = unsigned long, +X = unsigned long
+	int			ll; // +d = long long, +i = long long, +o = unsigned long long, +u = unsigned long long, +x = unsigned long long, +X = unsigned long long
 	int			L;
 
 	//conversions
