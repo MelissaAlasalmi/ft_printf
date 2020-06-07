528febd1b865a2855327d9c52f310ab005c3c018
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2020-06-07 19:18:34 +0300

    storing this here for now, reverting back to old itoabase file

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -1,47 +1,57 @@
 
 #include "ft_printf.h"
 
+int find_quotient(long value, long basestart, int quotient)
+{
+    while (value >= basestart)
+    {
+        quotient++;
+        value = value - basestart;
+    }
+    return(quotient);
+}
+
+long find_basestart(long value, int base, long basestart)
+{
+    while (value > (basestart * base))
+        basestart = basestart * base;
+    return(basestart);
+}
+
 char	*ft_itoa_base(long value, int base)
 {
 	char	*str;
 	int		i;
     long    basestart;
-    int     modulo;
+    int     quotient;
 
 	i = ft_intlen(value);
     basestart = 1;
-    modulo = 0;
+    quotient = 0;
 	if (!(str = (char *)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
 	str[i] = '\0';
 	i = 0;
     if (value == 0)
-		str[0] = 48;
+		str[0] = '0';
 	else if (value < 0)
     {
 		str[0] = '-';
         i = 1;
     }
     else
-    {
-        while (value > (basestart * base))
-            basestart = basestart * base;
-    }
+        find_basestart(value, base, basestart);
 	while (value >= 0 && basestart > 0)
 	{
         if (value < basestart)
-		    str[i] = 48;
+		    str[i] = '0';
         else
         {
-            while (value >= basestart)
-            {
-                modulo++;
-                value = value - basestart;
-            }
-            str[i] = modulo <= 9 ? '0' + modulo : 'a' + (modulo % 10);
+            find_quotient(value, basestart, quotient);
+            str[i] = quotient <= 9 ? '0' + quotient : 'a' + (quotient % 10);
         }
-        basestart = (basestart / base);  
-        modulo = 0;
+        basestart = (basestart / base); 
+        quotient = 0;
         i++;
 	}
 	return (str);
