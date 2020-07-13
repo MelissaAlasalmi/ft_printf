5f7f886a415553c15821f7bf679400480330d2db
Melissa Alasalmi <malasalm@c2r2p13.hive.fi>
2020-07-13 18:25:54 +0300

    i type now handling minus and width flags

--- types/type_i.c
+++ types/type_i.c
@@ -6,7 +6,7 @@
 /*   By: malasalm <malasalm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:26:21 by malasalm          #+#    #+#             */
-/*   Updated: 2020/07/12 21:05:46 by malasalm         ###   ########.fr       */
+/*   Updated: 2020/07/13 18:23:29 by malasalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,27 @@ void	type_i(va_list args, t_printf *data)
 
 	value = (unsigned long long)va_arg(args, void*);
 	sign_dec_int = ft_itoa(value);
-	ft_pf_putstr(sign_dec_int, data);
+	if (data->minus != 0)
+	{
+		ft_pf_putstr(sign_dec_int, data);
+		data->width = data->width - ft_strlen(sign_dec_int);
+		while (data->width > 0)
+		{
+			ft_pf_putchar(' ', data);
+			data->width--;
+		}
+	}
+	else if (data->width > (int)ft_strlen(sign_dec_int))
+	{
+		data->width = data->width - ft_strlen(sign_dec_int);
+		while (data->width > 0)
+		{
+			ft_pf_putchar(' ', data);
+			data->width--;
+		}
+		ft_pf_putstr(sign_dec_int, data);
+	}
+	else
+		ft_pf_putstr(sign_dec_int, data);
     //teststruct_during(data);
 }
