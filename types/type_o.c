547b697abb07857291b58d769d414e9aefca2b5d
Melissa Alasalmi <malasalm@c2r2p13.hive.fi>
2020-07-13 18:28:39 +0300

    o type now handling minus and width flags

--- types/type_o.c
+++ types/type_o.c
@@ -6,7 +6,7 @@
 /*   By: malasalm <malasalm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:26:27 by malasalm          #+#    #+#             */
-/*   Updated: 2020/07/12 21:06:06 by malasalm         ###   ########.fr       */
+/*   Updated: 2020/07/13 18:26:47 by malasalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,27 @@ void	type_o(va_list args, t_printf *data)
 
 	value = (unsigned long long)va_arg(args, void*);
 	base = ft_itoabase(value, 8);
-	ft_pf_putstr(base, data);
+	if (data->minus != 0)
+	{
+		ft_pf_putstr(base, data);
+		data->width = data->width - ft_strlen(base);
+		while (data->width > 0)
+		{
+			ft_pf_putchar(' ', data);
+			data->width--;
+		}
+	}
+	else if (data->width > (int)ft_strlen(base))
+	{
+		data->width = data->width - ft_strlen(base);
+		while (data->width > 0)
+		{
+			ft_pf_putchar(' ', data);
+			data->width--;
+		}
+		ft_pf_putstr(base, data);
+	}
+	else
+		ft_pf_putstr(base, data);
     //teststruct_during(data);
 }
