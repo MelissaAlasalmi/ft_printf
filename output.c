22ab783b4c3697a613df01634064abb9efe2a5e1
Melissa Alasalmi <malasalm@c1r4p8.hive.fi>
2020-07-22 17:33:27 +0300

    a few tweaks

--- output.c
+++ output.c
@@ -6,7 +6,7 @@
 /*   By: malasalm <malasalm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:25:58 by malasalm          #+#    #+#             */
-/*   Updated: 2020/07/22 15:01:21 by malasalm         ###   ########.fr       */
+/*   Updated: 2020/07/22 15:38:56 by malasalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,6 @@ t_printf	output_numerics(va_list args, t_printf *data)
 		type_X(args, data);
 	// else if (*data->nformat == 'f')
 	// 	type_f(args, data);
-	if (data->value < 0) // cancel out all of the flags that only work for positive numbers
-		data->plus = 0;
-	if (data->decimal == 1 && data->precision == 0) // cancel out precision if it's zero
-		data->decimal = 0;
 	return (*data);
 }
 
