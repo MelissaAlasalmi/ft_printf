dbb02c7eadfccca098392470b30bf864c323db78
Melissa Alasalmi <malasalm@c2r5p6.hive.fi>
2020-08-06 19:28:26 +0300

    some progress on type o, but the hash sign is still confusing to me. Need to read up on that more...

--- types/type_o.c
+++ types/type_o.c
@@ -6,7 +6,7 @@
 /*   By: malasalm <malasalm@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:26:27 by malasalm          #+#    #+#             */
-/*   Updated: 2020/08/06 18:13:43 by malasalm         ###   ########.fr       */
+/*   Updated: 2020/08/06 19:27:48 by malasalm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,25 @@ void	type_o(va_list args, t_printf *data)
 	base = ft_itoabase(data->value, 8);
 	if (data->value < 0 && data->plus == 1)
 		data->plus = 0;
-	if (data->value == 0 && (data->hash == 1 || (data->decimal == 1 && data->precision == 0)))
+	if (data->value == 0 && (data->hash == 1 || (data->decimal == 1 && data->precision == 0))) //I need to figure out how the heck has works, this is spaghetti!
+	{
 		base = "";
+		if (data->hash == 1)
+			data->width--;
+	}
 	if (data->plus == 1 || data->space == 1 || data->value < 0)
 		data->sign = 1;
 	if (data->minus == 1) // left justify!
 	{	
-		if (data->hash == 1 || data->sign == 1)
-		{
-			if (data->hash == 1)
-				ft_pf_putchar('0', data);
-			else
-				base = ft_putsign(data, base);
-			data->width--;
-		}
+		if (data->hash == 1)
+			ft_pf_putchar('0', data);
+		if (data->sign == 1)
+			base = ft_putsign(data, base);
 		if (data->precision < (int)ft_strlen(base))
-			data->width = data->width - ft_strlen(base);
+			data->width = data->width - (ft_strlen(base) + (data->sign + data->hash));
 		else
-			data->width = data->width - data->precision;
-		data->precision = data->precision - ft_strlen(base);
+			data->width = data->width - (data->precision + data->sign);
+		data->precision = data->precision - (ft_strlen(base) + data->hash);
 		ft_putzeros(data->precision, data);
 		ft_pf_putstr(base, data);
 		ft_putspaces(data->width, data);
@@ -64,7 +64,9 @@ void	type_o(va_list args, t_printf *data)
 			{
 				data->precision = (int)ft_strlen(base);
 				if (data->value < 0)
-					data->precision--;					
+					data->precision--;
+				if (data->hash == 1)		
+					data->precision++;	
 			}
 			ft_putspaces(data->width - (data->precision + data->sign), data);
 			if (data->sign == 1)
