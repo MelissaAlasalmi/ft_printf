86806e9b758fca68bccfc5c9cb8865c9fc22efb1
MelissaAlasalmi <melissa.alasalmi@gmail.com>
2020-08-16 19:48:09 +0300

    now handling colors. So cooooool.

--- ft_printf.h
+++ ft_printf.h
@@ -6,12 +6,22 @@
 /*   By: Melissa <Melissa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 14:25:39 by malasalm          #+#    #+#             */
-/*   Updated: 2020/08/16 19:24:59 by Melissa          ###   ########.fr       */
+/*   Updated: 2020/08/16 19:47:04 by Melissa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+
+# define BLACK				"\033[0:30m"
+# define RED 				"\033[0;31m"
+# define GREEN 				"\033[0;32m"
+# define YELLOW 			"\033[0;33m"
+# define BLUE 				"\033[0;34m"
+# define MAGENTA 			"\033[0;35m"
+# define CYAN 				"\033[0;36m"
+# define WHITE				"\033[0:37m"
+# define RESET 				"\033[0m"
 
 # include <unistd.h>
 # include <stdarg.h>
--- main.c
+++ main.c
@@ -5,5 +5,6 @@ int	main(void)
 	char *str;
 	str = "hello";
 
+	ft_printf(MAGENTA"hellllllo");
 	return (0);
 }
